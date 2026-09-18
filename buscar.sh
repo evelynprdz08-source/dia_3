3292a10f0453a508341fad0a211712185e4a149d
Evelyn Peña <evelyn.prdz08@gmail.com>
2026-09-17 18:43:27 -0600

    Agrega validacion de cantidad de argumentos

--- buscar.sh
+++ buscar.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# 1. Validar que se reciba un argumento (que no esté vacío)
-if [ -z "$1" ]; then
-    echo "Error: Debe proporcionar la ruta de una carpeta como argumento."
+if [ "$#" -gt 2 ]; then
+    echo "ERROR. Solo se permiten máximo 2 argumentos."
+    exit 1
+
+elif [ -z "$1" ]; then
+    echo "ERROR. Debe ingresar al menos la palabra a buscar."
     exit 1
 fi
-
-# 2. Validar que el argumento sea una carpeta existente
-if [ ! -d "$1" ]; then
-    echo "Error: '$1' no es una carpeta o no existe."
-    exit 1
-fi
-
-# 3. Lógica principal (si pasa las validaciones, hace la búsqueda)
-echo "Buscando archivos en la carpeta: $1..."
-grep -rl "home" "$1"
+PALABRA="$1"
 
